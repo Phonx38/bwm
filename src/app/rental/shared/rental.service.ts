f05b3dc97326e34f4ca97d475f9128412da7a0f3
Phonx38 <44253818+Phonx38@users.noreply.github.com>
2019-08-02 22:32:44 +0530

    adds pipes ,proxy,rental detail styles

--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,91 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable} from 'rxjs';
 import {Rental } from './rental.model';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable()
 
 export class RentalService {
 
-    private rentals: Rental[] = [{
-        id: 1,
-        title:"Central Apartment",
-        city: 'New York',
-        street: 'Times Square',
-        category: 'apartment',
-        image: 'http://via.placeholder,com/350*250',
-        bedrooms: 3,
-        description: 'Very Nice Apartment',
-        dailyRate: 34,
-        shared:false,
-        createdAt:"24/12/2015"
-      },
     
-        {
-          id: 2,
-          title:"Central Apartment 2",
-          city: 'london',
-          street: 'Times Square',
-          category: 'apartment',
-          image: 'http://via.placeholder,com/350*250',
-          bedrooms: 2,
-          description: 'Very Nice Apartment',
-          dailyRate: 54,
-          shared:false,
-          createdAt:"24/12/2015"
-        },
-    
-        {
-          id: 3,
-          title:"Central Apartment 3",
-          city: 'San francisco',
-          street: 'Times Square',
-          category: 'condo',
-          image: 'http://via.placeholder,com/350*250',
-          bedrooms: 3,
-          description: 'Very Nice Apartment',
-          dailyRate: 14,
-          shared: true,
-          createdAt:"24/12/2015"
-        },
-         
-        {
-          id: 4,
-          title:"Central Apartment 4",
-          city: 'Delhi',
-          street: 'Dwarka',
-          category: 'apartment',
-          image: 'http://via.placeholder,com/350*250',
-          bedrooms: 3,
-          description: 'Very Nice Apartment',
-          dailyRate: 34,
-          shared: true,
-          createdAt:"24/12/2015"
-        }];
+        constructor(private http:HttpClient) {}
 
-
-        public getRentalById(rentalId: number):Observable<Rental>{
-            return  new Observable<Rental>((observer)=>{
-                setTimeout(()=>{
-                   const foundRental= this.rentals.find((rental)=>{
-                        return rental.id== rentalId
-                    });
-                    observer.next(foundRental);
-                },500);
-            });
+        public getRentalById(rentalId: number):Observable<any >{
+            return this.http.get('/api/v1/rentals/'+ rentalId);
         }
 
-        public getRentals():Observable<Rental[]>{
-           return new Observable<Rental[]>((observer)=>{
+        public getRentals():Observable<any>{
 
-                setTimeout(()=>{
-                    observer.next(this.rentals);
-                },2000);
-            })
            
+            return this.http.get('/api/v1/rentals');
         }
 
-
-
-    
 
 }
